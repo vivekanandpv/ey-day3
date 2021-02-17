bc504ed4495bab44fc8fd4859b61c7b35e9f3e68
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-17 16:02:34 +0530

    element query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ey-day3';
+  @ViewChild('header')
+  headerElement!: ElementRef<HTMLElement>;
+
+  country = 'India';
+
+  ngAfterViewInit() {
+    this.headerElement.nativeElement.style.color = 'red';
+    this.headerElement.nativeElement.style.border = '1px solid blue';
+  }
 }
